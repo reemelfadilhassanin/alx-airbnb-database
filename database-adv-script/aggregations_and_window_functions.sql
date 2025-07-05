daad5ac6a1b1dcf82dd667e1bf9fb32b86bca7c9
reemelfadilhassanin <justreemjust49@gmail.com>
2025-07-05 05:17:47 +0200

    aggreation queries

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -3,9 +3,16 @@ SELECT user_id, COUNT(*) AS total_bookings
 FROM bookings
 GROUP BY user_id;
 
--- Rank properties based on total number of bookings using ROW_NUMBER
+-- Rank properties using ROW_NUMBER() based on total number of bookings
 SELECT property_id,
        COUNT(*) AS booking_count,
-       ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS property_rank
+       ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS property_row_number
+FROM bookings
+GROUP BY property_id;
+
+-- Rank properties using RANK() based on total number of bookings
+SELECT property_id,
+       COUNT(*) AS booking_count,
+       RANK() OVER (ORDER BY COUNT(*) DESC) AS property_rank
 FROM bookings
 GROUP BY property_id;
