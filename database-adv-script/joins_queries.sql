3a51183287d94567255004a6aa28f683d39c5b02
reemelfadilhassanin <justreemjust49@gmail.com>
2025-07-05 05:14:15 +0200

    aggreation queries

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -6,7 +6,8 @@ INNER JOIN users u ON b.user_id = u.id;
 -- LEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews
 SELECT p.id AS property_id, p.name AS property_name, r.id AS review_id, r.rating
 FROM properties p
-LEFT JOIN reviews r ON p.id = r.property_id;
+LEFT JOIN reviews r ON p.id = r.property_id
+ORDER BY p.id;
 
 -- FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
 SELECT u.id AS user_id, u.name AS user_name, b.id AS booking_id, b.start_date
